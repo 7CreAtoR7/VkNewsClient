7c5d30590e75b366b0960e36744164603a4761ab
Ilya Lisitskiy <ilyalisi7skiy@gmail.com>
2023-07-21 14:17:31 +0300

    adding bottom navigation in main screen

--- app/src/main/java/ru/ilya/vknewsclient/MainActivity.kt
+++ app/src/main/java/ru/ilya/vknewsclient/MainActivity.kt
@@ -10,6 +10,7 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import ru.ilya.vknewsclient.ui.theme.MainScreen
 import ru.ilya.vknewsclient.ui.theme.VkNewsClientTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,27 +18,8 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             VkNewsClientTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
-                }
+                MainScreen()
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    VkNewsClientTheme {
-        Greeting("Android")
     }
 }
